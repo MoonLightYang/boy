870a9a3a5d00480dd35ded11ad9ca78327d87388
MoonLightYang <729355767@qq.com>
2018-03-13 19:29:30 +0800

    123

--- src/boy/Test.java
+++ src/boy/Test.java
@@ -1,37 +1,33 @@
 package boy;
 
 public class Test {
-	
+
 	public static void main(String[] args) {
 		System.out.println(1);
 		int a = 1;
+		int b = 1;
 		int c = 0;
 		String s = "";
+		String x = "";
 		System.out.println("123");
 	}
-	
-	
-	public void nginx(){
+
+	public void my() {
+
+	}
+
+	public void nginx() {
 		System.out.println(1);
 		int a = 1;
-		String b = "c"; 
+		String b = "c";
 	}
-	
-	public void test(){
+
+	public void test() {
 		System.out.println(1);
 		int c = 1;
 		int e = 1;
 		int a = 1;
 		String b = "c";
 	}
-	
-	public void my() {
-		
-	}
-	
-	public void hello() {
-		System.out.println("123");
-	}
-	
 
 }
