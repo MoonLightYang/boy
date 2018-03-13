343eda47714fae41ccdfd1122160de4d735b067f
MoonLightYang <729355767@qq.com>
2018-03-13 17:43:41 +0800

    123

--- src/boy/Test.java
+++ src/boy/Test.java
@@ -3,11 +3,15 @@ package boy;
 public class Test {
 	
 	public static void main(String[] args) {
-		
+		System.out.println(1);
+		int c = 0;
+		String s = "";
 	}
 	
 	public void test(){
-		
+		System.out.println(1);
+		int a = 1;
+		String b = "c"; 
 	}
 
 }
