87358fce6e6baefb6d5e83358c744190fc1d7c01
liang.yang <562096674@qq.com>
2018-03-13 20:02:28 +0800

    123

--- src/boy/Test.java
+++ src/boy/Test.java
@@ -3,6 +3,10 @@ package boy;
 public class Test {
 	
 	public static void main(String[] args) {
+		System.out.println(1);
+		int a = 1;
+		int b = 1;
+		System.out.println("123");
 		System.out.println("123");
 	}
 	
