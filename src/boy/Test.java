dc6442115dd9506ccb76243c23412e62c3e1b60b
MoonLightYang <729355767@qq.com>
2018-03-13 19:22:34 +0800

    1

--- src/boy/Test.java
+++ src/boy/Test.java
@@ -4,20 +4,33 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println(1);
+		int a = 1;
 		int c = 0;
 		String s = "";
+		System.out.println("123");
+	}
+	
+	
+	public void nginx(){
+		System.out.println(1);
+		int a = 1;
+		String b = "c"; 
 	}
 	
 	public void test(){
 		System.out.println(1);
+		int c = 1;
 		int e = 1;
 		int a = 1;
 		String b = "c";
-		int c = 1;
 	}
 	
 	public void my() {
 		
+	}
+	
+	public void hello() {
+		System.out.println("123");
 	}
 
 }
