a2787498450eb76a7592cc24be8f8eb64f401148
liang.yang <562096674@qq.com>
2018-03-13 18:14:19 +0800

    123

--- src/boy/Test.java
+++ src/boy/Test.java
@@ -4,14 +4,28 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println(1);
+		int a = 1;
 		int c = 0;
 		String s = "";
+		System.out.println("123");
+	}
+	
+	
+	public void nginx(){
+		System.out.println(1);
+		int a = 1;
+		String b = "c"; 
 	}
 	
 	public void test(){
 		System.out.println(1);
+		int c = 1;
 		int a = 1;
 		String b = "c"; 
+	}
+	
+	public void hello() {
+		System.out.println("123");
 	}
 
 }
