3a1e9f0ffd760d772b5beaf09e58952cffab5971
liang.yang <562096674@qq.com>
2018-03-13 19:49:44 +0800

    123

--- src/boy/Test.java
+++ src/boy/Test.java
@@ -3,19 +3,9 @@ package boy;
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println(1);
-		int a = 1;
-		int b = 1;
-		int c = 0;
-		String s = "";
-		String x = "";
 		System.out.println("123");
 	}
-
-	public void tt() {
-
-	}
-
+	
 	public void nginx() {
 		System.out.println(1);
 		int a = 1;
@@ -24,15 +14,6 @@ public class Test {
 
 	public void test() {
 		System.out.println(1);
-		int c = 1;
-		int e = 1;
-		int a = 1;
 		String b = "c";
 	}
-	
-	public void hello() {
-		System.out.println("123");
-	}
-	
-
 }
