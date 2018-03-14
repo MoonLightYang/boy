cddd72ccacba187edc1dfae914083c2e826f343a
MoonLightYang <729355767@qq.com>
2018-03-14 09:54:54 +0800

    123

--- src/boy/Test.java
+++ src/boy/Test.java
@@ -5,14 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println(1);
 		System.out.println("123");
+		int a = 1;
 		int c = 0;
 		System.out.println("123");
 	}
-	
-	public void nginx() {
-		System.out.println("123");
-		System.out.println(1);
-		int a = 1;
-		String b = "c";
-	}
+
 }
