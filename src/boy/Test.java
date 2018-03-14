d9ab29e7ddc954e87b0344b9268f69ca51d16d63
liang.yang <562096674@qq.com>
2018-03-14 09:59:49 +0800

    234

--- src/boy/Test.java
+++ src/boy/Test.java
@@ -1,12 +1,17 @@
 package boy;
 
 public class Test {
-	
+
 	public static void main(String[] args) {
 		System.out.println(1);
 		System.out.println("123");
+		int a = 1;
 		int c = 0;
 		System.out.println("123");
+	}
+	
+	public void test() {
+		System.out.println("aaaa");
 	}
 	
 	public void nginx() {
